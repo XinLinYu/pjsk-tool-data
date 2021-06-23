39a0d12cdd26b27053cf9b85881082509dda780c
XinLin <48984235+XinLinYu@users.noreply.github.com>
2021-06-23 10:28:41 +0800

    初始化结构

---
+++ src/main/java/com/projectsekai/mapper/PjskCardMapper.java
@@ -0,0 +1,11 @@
+package com.projectsekai.mapper;
+
+/**
+ * @author XinlindeYu
+ * @version 1.0
+ * @ClassName PjskCardMapper
+ * @Description
+ * @date 2021/6/23 0023 上午 10:26
+ **/
+public interface PjskCardMapper {
+}
